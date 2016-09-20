c4e98f005963e70c6ac4e10aa748713cde599c7d
exec <exec@mabinoger.de>
2016-09-20 20:38:13 +0200

    Added C# index test

---
+++ UsageTest/public/Neuer Ordner/csindex/index.cs
@@ -0,0 +1,15 @@
+﻿using NHttp;
+using Swebs.RequestHandlers.CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class CSharpIndexTest : IScript
+{
+	public string Render(HttpRequestEventArgs args)
+	{
+		return "C# index";
+	}
+}
