3c2da3173c122dbc1a559c910270e39104762b53
exec <exec@mabinoger.de>
2016-09-23 15:01:45 +0200

    Moved Swebs.Server code to HttpServer
    Tunneling everything between NHttp and Swebs is too difficult, I'll
    directly modify NHttp instead.

--- UsageTest/Program.cs
+++ UsageTest/Program.cs
@@ -20,9 +20,9 @@ namespace UsageTest
 			webConf.SourcePaths.Add("public");
 			webConf.AllowDirectoryListing = true;
 
-			var server = new Server(webConf);
-			server.HttpServer.RequestReceived += (s, e) => Console.WriteLine("[{0}] - {1}", e.Request.HttpMethod, e.Request.Path);
-			server.HttpServer.UnhandledException += (s, e) => Console.WriteLine("[Error] {0}", e.Exception);
+			var server = new HttpServer(webConf);
+			server.RequestReceived += (s, e) => Console.WriteLine("[{0}] - {1}", e.Request.HttpMethod, e.Request.Path);
+			server.UnhandledException += (s, e) => Console.WriteLine("[Error] {0}", e.Exception);
 			server.Start();
 
 			//Process.Start("http://127.0.0.1:8181");
