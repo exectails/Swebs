a79f4d22878f376d299bfd7bdbddc92d5ec5f1de
exec <exec@mabinoger.de>
2016-09-23 15:20:29 +0200

    Updated UsageTest for GetLocalPath

--- UsageTest/public/Neuer Ordner/csindex/index.cs
+++ UsageTest/public/Neuer Ordner/csindex/index.cs
@@ -1,10 +1,20 @@
 ﻿using Swebs;
 using Swebs.RequestHandlers.CSharp;
+using System.Text;
 
 public class CSharpIndexTest : Controller
 {
 	public override void Handle(HttpRequestEventArgs args, string requestPath, string localPath)
 	{
-		args.Response.Send("C# index");
+		var server = args.Context.Server;
+
+		var sb = new StringBuilder();
+		sb.AppendLine("C# index<br/>");
+		sb.AppendLine("Test 1: " + server.GetLocalPath("Neuer Ordner/csindex/index.cs") + "<br/>");
+		sb.AppendLine("Test 2: " + server.GetLocalPath("Neuer Ordner 2/csindex/index.cs") + "<br/>");
+		sb.AppendLine("Test 3: " + server.GetLocalPath("css") + "<br/>");
+		sb.AppendLine("Test 4: " + server.GetLocalPath("test.txt") + "<br/>");
+
+		args.Response.Send(sb.ToString());
 	}
 }
---
+++ UsageTest/public2/Neuer Ordner 2/csindex/index.cs
@@ -0,0 +1,14 @@
+﻿using Swebs;
+using Swebs.RequestHandlers.CSharp;
+using System.Text;
+
+public class CSharpIndexTest2 : Controller
+{
+	public override void Handle(HttpRequestEventArgs args, string requestPath, string localPath)
+	{
+		var sb = new StringBuilder();
+		sb.AppendLine("C# index<br/>");
+
+		args.Response.Send(sb.ToString());
+	}
+}
